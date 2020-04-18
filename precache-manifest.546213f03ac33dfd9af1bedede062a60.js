5790d955985385ba7c5844c4022a8ce7df565e0d
Reza Aghajani <rezaaghajani@protonmail.com>
2020-04-18 14:31:26 +0430

    Updates

---
+++ precache-manifest.546213f03ac33dfd9af1bedede062a60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d3e2133964a0637c596a2f95e379f7",
+    "url": "/react-based-todo-app/index.html"
+  },
+  {
+    "revision": "f968d2092b96c2c5ad3e",
+    "url": "/react-based-todo-app/static/css/main.4c7e8429.chunk.css"
+  },
+  {
+    "revision": "bb9738db4bb93b553223",
+    "url": "/react-based-todo-app/static/js/2.4257e90d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-based-todo-app/static/js/2.4257e90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f968d2092b96c2c5ad3e",
+    "url": "/react-based-todo-app/static/js/main.f72b1895.chunk.js"
+  },
+  {
+    "revision": "79ea2262f365c3d229e0",
+    "url": "/react-based-todo-app/static/js/runtime-main.746b5d5a.js"
+  }
+]);
